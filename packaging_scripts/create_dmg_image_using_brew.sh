2247ee17be9d172787351de05d56e22f296e11eb
Dilawar Singh <dilawars@ncbs.res.in>
2016-06-26 16:23:36 +0530

    Updated. Try on strudel.

--- packaging_scripts/create_dmg_image_using_brew.sh
+++ packaging_scripts/create_dmg_image_using_brew.sh
@@ -73,18 +73,18 @@ export PATH=${BREW_PREFIX}/bin:$PATH
 (
     cd $BREW_PREFIX
     if [ ! -f $BREW_PREFIX/bin/brew ]; then
-        curl -L https://github.com/Homebrew/homebrew/tarball/master | \
-            tar xz --strip 1 -C $BREW_PREFIX
+        curl -L https://github.com/Homebrew/homebrew/tarball/master |  tar xz --strip 1 -C $BREW_PREFIX
     else
         echo "[I] Brew exists. Not installing"
     fi
     echo "Copying moose.rb and moogli.rb"
-    cp -f $CURRDIR/../macosx/*.rb $BREW_PREFIX/Library/Formula/
+    rsync -azv --progress $CURRDIR/../macosx/*.rb $BREW_PREFIX/Library/Formula/
 
     # This even works without python.
     ## NOTE: DO NOT install matplotlib using brew unless also installing python
     ## using brew. Since we are going to uninstall later, use pip to install
     ## matplotlib and numpy.
+    $BREW update
     $BREW -v install homebrew/python/matplotlib --with-pyqt
     $BREW -v install homebrew/python/numpy
     $BREW link --overwrite matplotlib
