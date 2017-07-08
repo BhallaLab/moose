ef2078a48ec29633a82a1cab36bfea3655a7593d
Dilawar Singh <dilawars@ncbs.res.in>
2017-07-08 11:40:18 +0530

    Fixed the syntax causing failure.

--- moose-core/python/moose/moose_test.py
+++ moose-core/python/moose/moose_test.py
@@ -30,7 +30,7 @@ logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
         datefmt='%m-%d %H:%M',
-        filename = logfile_.name
+        filename = logfile_.name,
         filemode='w'
         )
 console = logging.StreamHandler()
