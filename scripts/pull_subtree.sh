b3625c0d51b7a336a59c4f18ccb3ac36b960917c
Dilawar Singh <dilawars@ncbs.res.in>
2018-02-28 13:32:12 +0530

    preparing release candidate for chennapoda.

--- scripts/pull_subtree.sh
+++ scripts/pull_subtree.sh
@@ -24,11 +24,11 @@ set -x
 if [[ `pwd` == *"/moose" ]]; then
 
     git subtree pull --prefix moose-core \
-        https://github.com/BhallaLab/moose-core master --squash
+        https://github.com/BhallaLab/moose-core chennapoda --squash
     git subtree pull --prefix moose-examples \
-        https://github.com/BhallaLab/moose-examples master --squash 
+        https://github.com/BhallaLab/moose-examples chennapoda --squash 
     git subtree pull --prefix moose-gui \
-        https://github.com/BhallaLab/moose-gui master --squash
+        https://github.com/BhallaLab/moose-gui chennapoda --squash
 
 else
     echo "Run this script from top-level git directory."
