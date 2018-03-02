33bf735fb61ae5a94a2a531fa14a03632ee7ca42
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-02 16:48:47 +0530

    Squashed 'moose-core/' changes from f1dcea8..f38dc5d
    f38dc5d On tumbleweed, cmake returns python3 default but python-numpy is python2 version. Won't fix it. Its a upstream problem.
    REVERT: f1dcea8 Install in debian layout in packages. Added option in setup.cfg file Its delicate solution. Added a note.
    REVERT: 5a73acb Finally. Needs to write setup.cfg file to make /usr/local  and /usr prefix consistent.
    REVERT: d7c41e4 bdist_egg is more consistent on both RPM and DEB based systems.
    REVERT: be42fd2 Merge commit 'bcf5c52d54409a89f9af1cf08e5d369d9e9e0d7f' into chennapoda
    REVERT: d27db9c Do not use -p switch. its not easy to get required info from all linux-distributions.
    REVERT: e667953 Fixed message to set in cmake.
    REVERT: 8523290 Fixed typo in command.
    REVERT: 0b125f6 Fixed typo in cmake file.
    REVERT: f3118f0 multiple fix to build system. - dont use python setup.py install . It does not work anymore if --prefix   directory is not in PYTHONPATH. - Use python setup.py bdist -d INSTALL_DIR and let cmake unarchive and copy it   to desired location. This is much better solution.
    REVERT: 03d5899 Use bdist instead of install for creating binay distrution. During installation, untar this to destination.
    REVERT: a08658e Tweaks to setup.cmake.py file.
    REVERT: c5f1d91 Test on travis.
    REVERT: 6b32fde Merge commit '6d651b6013e1093a1cdb2541bb8615bbc3a66021'
    REVERT: 324a20c Merge commit 'e0b8959ccefd77195b7eac2edc1db7cdcaa40fe6'
    REVERT: 59f6e9e On debian/ubuntu install-layout option is set to deb. [skip ci]
    REVERT: 62201d9 Merge commit 'f80ae40d5b1e1b63eb9849b9699d76f33e4f2572'
    REVERT: 9b81dfc This fixes moose but a better solution to install pymoose in moose-core must be found.
    REVERT: 7e27f80 Merge commit '6f7697fb3256626a6d57bda24ae0a7bb5c0e90cc'
    REVERT: 939c734 Merge commit '627f1ce1cc73b0e1f6b4fe9051d3597e3a9113a5'
    REVERT: 4775933 Merge commit '7b3c355a04d22f279a88bfae4097e9cf3f62439b'
    REVERT: 43c0674 Squashed 'moose-core/' changes from d4417dcffd..2657e3345e (#222)
    REVERT: 8c9ad49 Fixed the syntax causing failure.
    REVERT: 397efaf Handling MOOSE_VERSION is better way.
    
    git-subtree-dir: moose-core
    git-subtree-split: f38dc5d6b2d6a3bcb5439a2f5ba680adad754ec7

--- python/moose/moose_test.py
+++ python/moose/moose_test.py
@@ -26,12 +26,11 @@ import logging
 from collections import defaultdict
 import time
 
-logfile_ = tempfile.NamedTemporaryFile( )
 logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
         datefmt='%m-%d %H:%M',
-        filename = logfile_.name,
+        filename='tests.log',
         filemode='w'
         )
 console = logging.StreamHandler()
