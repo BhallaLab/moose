333090e6b776cd599cc498fb8ebfcdeb0e7b1044
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-05 15:46:19 +0530

    Squashed 'moose-core/' changes from 411a71798a..d46235155d
    d46235155d no chemmerge module in this release.
    83b01b2c2d Passes locally.
    39d26ac077 Fixed the location of .so path with python3.
    9fe69ca3c2 updated version in wheel file.
    7cd0ff82ec renamed setup.py to setup.cmake.py .
    f849357bc0 Merge pull request #257 from BhallaLab/3.1.3
    1bdcd2e294 Use 3 threads and don't select python from /opt on travis.
    f38086baf3 changes to .travis.yaml. OSX build is failing.
    d48f79d52e Test the brew formulae in bhallalab/homebrew-moose repo.
    8823b7920b ensurepip and use pip as python -m pip.
    8a3d67517e Merge branch '3.1.3' of https://github.com/BhallaLab/moose-core into 3.1.3
    b69b01d958 updated travis osx scripts on 3.1.3 release.
    3b000e1718 Merge branch '3.1.3' of github.com:BhallaLab/moose-core into 3.1.3 [skip ci]
    330b50b764 This passes on local linux. Deleted old cmake test file. [skip ci]
    9c5a786787 version 3.1.3.
    53213c8d37 merged travis scripts from master.
    c42efd4bbe removed moogli as well.
    2e05726e43 Removed two ancient tests; they are not relevant.
    4dd18210cc Updated cmake files from chennapoda branch.
    26716cc191 Removed legacy makefile based build from chamcham. Its failing on travis because of unique_ptr .
    3516e32770 python3.6 related fix. auto_ptr -> unique_ptr.
    50c57b3f21 Fixed to #204. Added test to sanity test moose.doc Fixed some issue raised by pre-commit hooks. Fixed moose.doc( ) call.
    6a4b102099 Updated pre-commit hooks in chamcham branch.
    
    git-subtree-dir: moose-core
    git-subtree-split: d46235155d281b30f76746e7c38dda1e84402a5a

--- .travis/travis_build_linux.sh
+++ .travis/travis_build_linux.sh
@@ -1,12 +1,12 @@
-#!/bin/bash - 
+#!/usr/bin/env bash
 #===============================================================================
 #
 #          FILE: travis_build_linux.sh
-# 
-#         USAGE: ./travis_build_linux.sh 
-# 
+#
+#         USAGE: ./travis_build_linux.sh
+#
 #   DESCRIPTION:  Build  on linux environment.
-# 
+#
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
@@ -22,32 +22,29 @@ set -e
 
 PYTHON2="/usr/bin/python2"
 PYTHON3="/usr/bin/python3"
-#MAKEFLAGS="-j 4"
+MAKEFLAGS="-j4"
 
 # Bug: `which python` returns /opt/bin/python* etc on travis. For which numpy
 # many not be available. Therefore, it is neccessary to use fixed path for
 # python executable.
 
 (
-    # Old makefile based flow.
-    python2 -m compileall -q .
-    if type python3 -c 'import sys' > /dev/null; then python3 -m compileall -q . ; fi
-    # Traditional make.
-    make 
-    ## CMAKE based flow
+    $PYTHON2 -m compileall -q .
+    if type $PYTHON3 > /dev/null; then $PYTHON3 -m compileall -q . ; fi
+
     mkdir -p _GSL_BUILD && cd _GSL_BUILD && \
         cmake -DDEBUG=ON -DPYTHON_EXECUTABLE="$PYTHON2" ..
     make && ctest --output-on-failure
     cd .. # Now with boost.
     mkdir -p _BOOST_BUILD && cd _BOOST_BUILD && \
-        cmake -DWITH_BOOST=ON -DDEBUG=ON -DPYTHON_EXECUTABLE="$PYTHON2" ..
+        cmake -DWITH_BOOST=ON -DDEBUG=ON -DQUIET_MODE=ON -DPYTHON_EXECUTABLE="$PYTHON2" ..
     make && ctest --output-on-failure
-    cd .. 
+    cd ..
 
     # This is only applicable on linux build.
-    echo "Python3 support. Removed python2-networkx and install python3" 
-    if type $PYTHON3 -c 'import os' > /dev/null; then 
-        sudo apt-get remove -qq python-networkx 
+    echo "Python3: Removed python2-networkx and install python3"
+    if type $PYTHON3 > /dev/null; then
+        sudo apt-get remove -qq python-networkx
         sudo apt-get install -qq python3-networkx
         mkdir -p _GSL_BUILD2 && cd _GSL_BUILD2 && \
             cmake -DDEBUG=ON -DPYTHON_EXECUTABLE="$PYTHON3" ..
