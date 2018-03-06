06aeb7c158b9d019684b67a8e0b03a3ea7da0cb9
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-06 12:03:15 +0530

    Add log to a file.

--- .travis_build.sh
+++ .travis_build.sh
@@ -20,5 +20,5 @@
 set -o nounset                                  # Treat unset variables as an error
 set -e -x
 PATH=/usr/bin:/usr/local/bin:$PATH
-gbp buildpackage  --git-ignore-branch --git-ignore-new -uc -us -d
+gbp buildpackage  --git-ignore-branch --git-ignore-new -uc -us -d | tee _gbp.log
 pwd 
