db82109d3ee455ba8224fcd1396d190988a53736
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-06 18:47:51 +0530

    got changes from chamcham.

---
+++ .travis_build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: .travis_build.sh
+#
+#         USAGE: ./.travis_build.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
+#  ORGANIZATION: NCBS Bangalore
+#       CREATED: 03/05/2018 03:49:35 PM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+set -e -x
+PATH=/usr/bin:/usr/local/bin:$PATH
+gbp buildpackage  --git-ignore-branch --git-ignore-new -uc -us -d | tee _gbp.log
+pwd 
