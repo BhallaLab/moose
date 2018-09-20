0e5013935b8171d5954ec980caa042f823738222
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-20 13:27:23 +0530

    Some test only runs on travis only.

--- .travis_build_linux.sh
+++ .travis_build_linux.sh
@@ -4,3 +4,11 @@ set -e -x
 PATH=/usr/bin:/usr/local/bin:$PATH
 gbp buildpackage  --git-ignore-branch --git-ignore-new -uc -us -d | tee _gbp.log
 pwd 
+ls ../*.deb
+
+if [ -n "$TRAVIS" ]; then
+    echo "We are on travis"
+    sudo dpkg -i ../*.deb
+    sudo apt-get install -f 
+    python -c 'import moose; print(moose.__file__); print(moose.__version__)'
+fi
