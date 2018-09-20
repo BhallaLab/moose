9080fc1740f9ed6bf8d8506d77c9fa1b5552d127
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-20 15:34:24 +0530

    Don't run moose.test().

--- .travis_build_osx.sh
+++ .travis_build_osx.sh
@@ -3,4 +3,3 @@ brew tap BhallaLab/moose
 brew install moose
 python -m pip install networkx python-libsbml pyNeuroML  matplotlib
 python -c 'import moose; print( moose.__version__ )'
-python -c 'import moose; print( moose.test() )'
