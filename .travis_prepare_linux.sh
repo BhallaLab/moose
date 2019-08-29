abda53f316d3146728ad5bd1d7b2bdf8de548e72
Dilawar Singh <dilawar@users.noreply.github.com>
2019-08-29 15:24:39 +0530

    Update .travis_prepare_linux.sh
    Added fakeroot package

--- .travis_prepare_linux.sh
+++ .travis_prepare_linux.sh
@@ -8,6 +8,6 @@ sudo apt-get -y install libgsl0-dev
 sudo apt-get -y install python-suds python-matplotlib 
 sudo apt-get -y install python-qt4-dev python-qt4-gl python-sip-dev libqt4-dev libopenscenegraph-dev
 sudo apt-get -y install doxygen python-sphinx python-lxml
-sudo apt-get -y install git-buildpackage
+sudo apt-get -y install git-buildpackage fakeroot
 sudo apt-get -y install -f
 sudo ldconfig /usr/lib64
